9a108d3cb2d12a9e6769d144b6468a07f143a0a4
hellboy28D <yadavdayaram.nuh@gmail.com>
2026-08-31 12:13:03 +0530

    Initial commit

--- src/train.py
+++ src/train.py
@@ -12,3 +12,13 @@ num_example_to_generator = 16
 seed = tf.random.normal([num_example_to_generator, noise_dim])
 
 # Notice the use of 'tf.function'
+# This annotation causes the function to be "compiled".
+@tf.function
+def train_step(images):
+    noise =tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training = True)
+
+        real_output = discriminator(images, training = True)
+        fake_output = discriminator(generated_images, training = True)
