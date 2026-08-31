2fd7c2a160680ca8b44c936a5e647f1a2bd3c6af
hellboy28D <yadavdayaram.nuh@gmail.com>
2026-08-31 11:56:52 +0530

    Initial commit

--- src/preprocess.py
+++ src/preprocess.py
@@ -75,6 +75,16 @@ print(decision)
 # *** Loss and Optimizer ***
 # This method returns a helper function to compute cross entropy loss
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True)
-
+tf.ones_like((1,1,1,1,1,1,1,1,1,0,0))
 
 # Dicrimnator Loss
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.one_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+# Generator Loss
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
