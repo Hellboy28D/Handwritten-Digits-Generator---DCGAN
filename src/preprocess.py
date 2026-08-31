5a5f51c2a1ec56d8eda8fa2addfabe6368962fdb
hellboy28D <yadavdayaram.nuh@gmail.com>
2026-08-31 11:46:51 +0530

    Initial commit

--- src/preprocess.py
+++ src/preprocess.py
@@ -46,3 +46,22 @@ generated_image = generator(noise, training = False)
 
 plt.imshow(generated_image[0, :, :, 0], cmap='gray')
 
+# Discriminator
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64,(5,5), strides=(2,2), padding = 'same',
+                                    input_shape=[28,28,1]))
+    
+    model.add(layers.LeakReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128,(5,5), strides=(2,2), padding='same'))
+    model.add(layers.LeakReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
